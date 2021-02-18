324ff180cb96b6436f73084eeccd57033772aab8
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-02-18 23:04:56 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -43,22 +43,22 @@ class Game
     end
   end
 
-    def turn
-      player = current_player
-      current_move = player.move(@board)
-      if !@board.valid_move?(current_move)
-        turn
-      else
-        puts "Turn: #{@board.turn_count+1}\n"
-        @board.display
-        @board.update(current_move, player)
-        puts "#{player.token} moved #{current_move}"
-        @board.display
-        puts "\n\n"
-      end
+  def turn
+    player = current_player
+    current_move = player.move(@board)
+    if !@board.valid_move?(current_move)
+      turn
+    else
+      puts "Turn: #{@board.turn_count+1}\n"
+      @board.display
+      @board.update(current_move, player)
+      puts "#{player.token} moved #{current_move}"
+      @board.display
+      puts "\n\n"
     end
+  end
 
-    def play
+  def play
     while !over?
       turn
     end
@@ -66,42 +66,42 @@ class Game
       puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
-  def won?
-    WIN_COMBINATIONS.detect do |combo|
-      @board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[1]] == @board.cells[combo[2]] && @board.taken?(combo[0]+1)
-    end
-  end
-
-  def draw?
-    true if !won? && @board.full?
-  end
-
-  def over?
-    true if won? || @board.full? || draw?
-  end
-
-  def winner
-    WIN_COMBINATIONS.detect do |combo|
-      if @board.cells[combo[0]] == "X" && @board.cells[combo[1]] == "X" && @board.cells[combo[2]] == "X"
-        return "X"
-      elsif
-        @board.cells[combo[0]] == "O" && @board.cells[combo[1]] == "O" && @board.cells[combo[2]] == "O"
-          return "O"
+      def won?
+        WIN_COMBINATIONS.detect do |combo|
+          @board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[1]] == @board.cells[combo[2]] && @board.taken?(combo[0]+1)
+        end
       end
-    end
-  end
 
-  def turn
-    puts "Please enter 1-9:"
-    input = @player_1.move(@board)
-    index = @board.to_index(input)
-    player = current_player
-    if @board.valid_move?(index)
-      @board.update(index, player)
-      @board.display_board
-    else
-      turn
-    end
-  end
+      def draw?
+        true if !won? && @board.full?
+      end
 
-end
+      def over?
+        true if won? || @board.full? || draw?
+      end
+
+      def winner
+        WIN_COMBINATIONS.detect do |combo|
+          if @board.cells[combo[0]] == "X" && @board.cells[combo[1]] == "X" && @board.cells[combo[2]] == "X"
+            return "X"
+          elsif
+            @board.cells[combo[0]] == "O" && @board.cells[combo[1]] == "O" && @board.cells[combo[2]] == "O"
+            return "O"
+          end
+        end
+      end
+
+      def turn
+        puts "Please enter 1-9:"
+        input = @player_1.move(@board)
+        index = @board.to_index(input)
+        player = current_player
+        if @board.valid_move?(index)
+          @board.update(index, player)
+          @board.display_board
+        else
+          turn
+        end
+      end
+
+    end
