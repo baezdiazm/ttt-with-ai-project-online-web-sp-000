c599ce3fad54b3049e39a861c000ee2842d7f9c8
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-02-18 23:04:27 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -13,7 +13,11 @@ class Game
   end
 
   def current_player
-    @board.turn_count % 2 == 0 ? @player_1 : @player_2
+    if @board.turn_count % 2 == 0
+      @player_1
+    else
+      @player_2
+    end
   end
 
   def over?
