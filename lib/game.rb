c6effb7e62476fa01a7da9ab7a5447c7c92d99b2
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-02-18 23:07:44 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -66,6 +66,7 @@ class Game
       puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
+    end
       def won?
         WIN_COMBINATIONS.detect do |combo|
           @board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[1]] == @board.cells[combo[2]] && @board.taken?(combo[0]+1)
