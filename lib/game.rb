1cdc4eee374bf987e0d8a1814593471694ca2528
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-02-18 23:09:35 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -13,11 +13,7 @@ class Game
   end
 
   def current_player
-    if @board.turn_count % 2 == 0
-      @player_1
-    else
-      @player_2
-    end
+    @board.turn_count % 2 == 0 ? @player_1 : @player_2
   end
 
   def over?
@@ -43,22 +39,22 @@ class Game
     end
   end
 
-  def turn
-    player = current_player
-    current_move = player.move(@board)
-    if !@board.valid_move?(current_move)
-      turn
-    else
-      puts "Turn: #{@board.turn_count+1}\n"
-      @board.display
-      @board.update(current_move, player)
-      puts "#{player.token} moved #{current_move}"
-      @board.display
-      puts "\n\n"
+    def turn
+      player = current_player
+      current_move = player.move(@board)
+      if !@board.valid_move?(current_move)
+        turn
+      else
+        puts "Turn: #{@board.turn_count+1}\n"
+        @board.display
+        @board.update(current_move, player)
+        puts "#{player.token} moved #{current_move}"
+        @board.display
+        puts "\n\n"
+      end
     end
-  end
 
-  def play
+    def play
     while !over?
       turn
     end
@@ -67,43 +63,6 @@ class Game
     elsif draw?
       puts "Cat's Game!"
     end
-
-    def won?
-      WIN_COMBINATIONS.detect do |combo|
-        @board.cells[combo[0]] == @board.cells[combo[1]] && @board.cells[combo[1]] == @board.cells[combo[2]] && @board.taken?(combo[0]+1)
-      end
-    end
-
-    def draw?
-      true if !won? && @board.full?
-    end
-
-    def over?
-      true if won? || @board.full? || draw?
-    end
-
-    def winner
-      WIN_COMBINATIONS.detect do |combo|
-        if @board.cells[combo[0]] == "X" && @board.cells[combo[1]] == "X" && @board.cells[combo[2]] == "X"
-          return "X"
-        elsif
-          @board.cells[combo[0]] == "O" && @board.cells[combo[1]] == "O" && @board.cells[combo[2]] == "O"
-          return "O"
-        end
-      end
-    end
-
-    def turn
-      puts "Please enter 1-9:"
-      input = @player_1.move(@board)
-      index = @board.to_index(input)
-      player = current_player
-      if @board.valid_move?(index)
-        @board.update(index, player)
-        @board.display_board
-      else
-        turn
-      end
-    end
-
   end
+
+end
